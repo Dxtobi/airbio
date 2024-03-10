6e2a552e4b94c99f1a8d7e0fa18e1a14b99680c8
Dxtobi <akanbijosephtobi@gmail.com>
2024-03-10 00:42:53 -0800

    OPTIMIZED: image size

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,14 +5,16 @@ export const prerender = false;
 export const load = async ({ locals }) => {
 
     // const users = await locals.pb.collection('user').getList();
-    const resumes = await locals.pb.collection('resumes').getList();
-    const users = await locals.pb.collection('users').getList();
+    // const resumes = await locals.pb.collection('resumes').getList();
+    // const users = await locals.pb.collection('users').getList();
 
+
+    //would come back to this.
 
     return {
         counts: {
-            users: users?.totalItems,
-            resumes: resumes?.totalItems,
+            users: 8,//users?.totalItems,
+            resumes: 7// resumes?.totalItems,
         }
     }
 }
